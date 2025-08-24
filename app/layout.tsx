767b0a1e7582058fed5c855e5255f572f7117842
Dario George <edu.dariogeorge21@gmail.com>
2025-08-24 22:46:30 +0530

    favicon: route

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Bus Pass Booking - St. Joseph\'s College of Engineering and Technology, Palai',
   description: 'Online bus pass booking system for students',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
